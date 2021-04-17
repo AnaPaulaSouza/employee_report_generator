7875614666e004c6a463ed33e486c2f527ad028a
Ana Paula Souza <ana.pdsouza@b2wdigital.com>
2021-04-17 19:43:59 -0300

    first commit

---
+++ lib/employee_report_generator.ex
@@ -0,0 +1,18 @@
+defmodule EmployeeReportGenerator do
+  @moduledoc """
+  Documentation for `EmployeeReportGenerator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EmployeeReportGenerator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/employee_report_generator_test.exs
@@ -0,0 +1,8 @@
+defmodule EmployeeReportGeneratorTest do
+  use ExUnit.Case
+  doctest EmployeeReportGenerator
+
+  test "greets the world" do
+    assert EmployeeReportGenerator.hello() == :world
+  end
+end
